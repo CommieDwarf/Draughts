0e7190de7583abf0c5b61d41347038e0b5c40693
CommieDwarf <tosiek22@gmail.com>
2025-05-17 17:16:53 +0200

    remove unnecessary files

--- client/mid/components/letters.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
-exports.default = letters;
-//# sourceMappingURL=letters.js.map
